cbd20475949f688f4c4204007645c69106e6bbe2
Elaina-AFK <khglhk@yahoo.com>
2021-12-28 17:06:04 +0700

    lab14_1.cpp edited

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,36 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double B[], int N, double ans[]){
+    double a = 0; 
+    for(int i = 0; i < N; i++){
+        a += B[i];
+    }
+    ans[0] = a / N;
+    a = 0;
+    for(int i = 0; i < N; i++){
+        a += pow(B[i] - ans[0], 2); 
+    }
+    ans[1] = sqrt(a / N); 
+    a = 1;
+    for(int i = 0; i < N; i++){
+        a *= B[i];
+    }
+    ans[2] = pow(a, 1.0 / N);
+    a = 0;
+    for(int i = 0; i < N; i++){
+        a += 1.0 / B[i];
+    }
+    ans[3] = N / a;
+    a = B[0];
+    for(int i = 1; i < N; i++){
+        if(a < B[i]) a = B[i];
+    }
+    ans[4] = a;
+    a = B[0];
+    for(int i = 1; i < N; i++){
+        if(a > B[i]) a = B[i];
+    }
+    ans[5] = a;
+}
